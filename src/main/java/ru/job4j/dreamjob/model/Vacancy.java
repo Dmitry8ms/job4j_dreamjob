3b11c59352c2539441321bfb91ef063cb485585a
Dmitry8ms <dshatokhin@yandex.ru>
2022-12-25 20:10:42 +0700

    4. Thymeleaf, Циклы. [#504841 #372404]

---
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -0,0 +1,68 @@
+package ru.job4j.dreamjob.model;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Vacancy {
+
+    private int id;
+    private String title;
+    private String description;
+    private LocalDateTime created;
+
+    public Vacancy(int id, String title, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Vacancy vacancy = (Vacancy) o;
+        return id == vacancy.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+}
