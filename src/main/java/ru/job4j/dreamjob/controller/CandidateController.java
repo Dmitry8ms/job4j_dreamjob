609f5d2c08b838f565c6a6e337117db237a45c6f
Dmitry8ms <dshatokhin@yandex.ru>
2023-01-31 12:40:52 +0700

    9. HTML form. Создание страницы кандидата [#504845]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -16,4 +16,8 @@ public class CandidateController {
         model.addAttribute("candidates", candidateRepository.findAll());
         return "candidates/list";
     }
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "candidates/create";
+    }
 }
